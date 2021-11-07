236a9def4bdd8bcdd92fcd14ac6eb467fc483d34
Edison Abdiel <edison.abdiel@soundcloud.com>
2021-11-07 22:33:56 +0100

    feat: Add Header. style: Link globa.sass and tailwindcss

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b331e5aa44203378"; }
+/******/ 		__webpack_require__.h = function() { return "c874d0564a68f4ec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Layout } from "../components";
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
-import { ThemeProvider } from 'next-themes';
+import { ThemeProvider } from "next-themes";
+import "../styles/globals.scss";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-    return (
-        <ThemeProvider attribute="media class">
-            <Component {...pageProps} />
-        </ThemeProvider>
-    )
+  return (
+    <ThemeProvider attribute="media class">
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
